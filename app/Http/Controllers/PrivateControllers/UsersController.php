04a76540b563143bf367a3a7343e8163d9a2cdd3
karl <fares_k@etna-alternance.net>
2018-05-15 17:11:26 +0100

    dev

--- app/Http/Controllers/PrivateControllers/UsersController.php
+++ app/Http/Controllers/PrivateControllers/UsersController.php
@@ -77,9 +77,11 @@ class UsersController extends Controller
 	}
 
 	public function logout(Request $request) {
-		var_dump("LOGOUT");
+		var_dump("LOGOUT-0");
 		$tokenMdl = UsersToken::getFromHeader($request);
+		var_dump("LOGOUT-1");
 		
+		exit;
 		if (!$tokenMdl) {
 			var_dump("No Model Found");
 			exit;
