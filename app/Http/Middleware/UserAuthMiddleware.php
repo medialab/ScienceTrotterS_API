1435620bbab67b6ee232c20b2310f4b77f0817e2
karl <fares_k@etna-alternance.net>
2018-05-15 17:06:17 +0100

    dev

--- app/Http/Middleware/UserAuthMiddleware.php
+++ app/Http/Middleware/UserAuthMiddleware.php
@@ -51,6 +51,8 @@ class UserAuthMiddleware extends Controller
     	$validationData->setIssuer('http://'.$_SERVER['HTTP_HOST']);
     	$validationData->setAudience('http://'.$_SERVER['HTTP_HOST']);
     	$validationData->setId(UsersToken::idfyUser($user));
+    	$validationData->setCurrentTime(time() + 60);
+
     	var_dump("Generated ID: ".UsersToken::idfyUser($user));
 
     	if (!$token->validate($validationData)) {
