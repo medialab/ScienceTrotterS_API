fdf40ebd784f54167aff659d3834f2697b6b995b
karl <fares_k@etna-alternance.net>
2018-05-15 15:27:53 +0100

    dev

--- app/Http/Controllers/PrivateControllers/UsersController.php
+++ app/Http/Controllers/PrivateControllers/UsersController.php
@@ -45,7 +45,7 @@ class UsersController extends Controller
 		]);
 
 		$user = Users::where('email', $request->input('email'))->first();
-		 
+		 var_dump($user);
 	    if(Hash::check($request->input('password'), $user->password)){
 			$token = new UsersToken($this);
 			exit;
