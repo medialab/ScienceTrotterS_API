dc4319b375e3c36f43f2255619189e33c508dc90
karl <fares_k@etna-alternance.net>
2018-05-16 15:41:37 +0100

    update

--- routes/public/cities.php
+++ routes/public/cities.php
@@ -1,4 +1,14 @@
 <?php
 
+use App\Models\Cities;
 $router->get('/list', 'CitiesController@list');
-$router->get('/{id:[a-z0-9-]+}', 'CitiesController@find');
+
+$router->get('/{id:[a-z0-9-]+}', function($id) use ($router) {
+	$this->validate($request, [
+		'id' => 'required',
+	]);
+
+	$oCity = Cities::where('id', $oRequest->input('id'))->first();
+	echo json_encode($oCity->toArray());
+	exit;
+});
