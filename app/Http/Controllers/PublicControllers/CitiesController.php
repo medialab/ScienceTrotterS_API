3313913eecda97ca547243c04501d48af9f0109a
karl <fares_k@etna-alternance.net>
2018-05-16 17:10:20 +0100

    update

--- app/Http/Controllers/PublicControllers/CitiesController.php
+++ app/Http/Controllers/PublicControllers/CitiesController.php
@@ -12,6 +12,7 @@ class CitiesController extends Controller
 	public function list(Request $oRequest)
 	{
 		$aCities = Cities::where('state', true)->take($oRequest->input('limit'))->skip($oRequest->input('offset'));
+		var_dump($aCities);
 		return $this->sendResponse($aCities->toArray(), null);
 	}
 
