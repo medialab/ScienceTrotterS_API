336589a4274c1ed2dba44073b8ea658494a25802
karl <fares_k@etna-alternance.net>
2018-05-18 14:47:03 +0100

    dev

--- app/Http/Controllers/PublicControllers/CitiesController.php
+++ app/Http/Controllers/PublicControllers/CitiesController.php
@@ -6,6 +6,7 @@ use App\Utils\APIControllerUtil as Controller;
 use App\Utils\RequestUtil as Request;
 use App\Utils\ValidatorUtil as Validator;
 use App\Models\Cities;
+use App\Models\Parcours;
 
 class CitiesController extends Controller
 {
@@ -17,6 +18,21 @@ class CitiesController extends Controller
 
 	public function get($id) {
 		$oCity = Cities::where('id', $id)->first();
+		return $this->sendResponse($oCity->toArray(), null);
+	}
+}
+
+
+class ParcoursController extends Controller
+{
+	public function list(Request $oRequest)
+	{
+		$aParcours = Parcours::where('state', true)->take($oRequest->input('limit'))->skip($oRequest->input('offset'))->get();
+		return $this->sendResponse($aParcours->toArray(), null);
+	}
+
+	public function get($id) {
+		$oCity = Parcours::where('id', $id)->first();
 		return $this->sendResponse($oCity->toArray(), null);
 	}
 }
