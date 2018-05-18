c82b77bfc2a11f5ed425eaf313663b954269177d
karl <fares_k@etna-alternance.net>
2018-05-18 15:03:23 +0100

    dev

--- routes/public/parcours.php
+++ routes/public/parcours.php
@@ -1,4 +1,17 @@
 <?php
+$p = dirname(__FILE__).'/../../app/Http/Controllers/PublicControllers/ParcoursController.php';
+require_once($p);
 
-$router->get('/list', 'TestController@list');
+use App\Http\Controllers;
+use App\Http\Controllers\Test as Test;
+
+
+
+var_dump($p);
+var_dump(file_exists($p));
+
+var_dump(class_exists("Test"));
+exit;
+
+$router->get('/list', 'ParcoursController@list');
 $router->get('/{id:[a-z0-9-]+}', 'CitiesController@get');
