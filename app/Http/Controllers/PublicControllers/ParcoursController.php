0a7711fb0d53c0a8a3f72bf878ca768c63bfc5ca
karl <fares_k@etna-alternance.net>
2018-05-18 14:56:05 +0100

    dev

--- app/Http/Controllers/PublicControllers/ParcoursController.php
+++ app/Http/Controllers/PublicControllers/ParcoursController.php
@@ -1,12 +1,5 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use App\Utils\APIControllerUtil as Controller;
-use App\Utils\RequestUtil as Request;
-use App\Utils\ValidatorUtil as Validator;
-use App\Models\Parcours;
-
 /**
  * 
  */
@@ -19,16 +12,3 @@ class Test
 	}
 }
 
-class ParcoursController extends Controller
-{
-	public function list(Request $oRequest)
-	{
-		$aParcours = Parcours::where('state', true)->take($oRequest->input('limit'))->skip($oRequest->input('offset'))->get();
-		return $this->sendResponse($aParcours->toArray(), null);
-	}
-
-	public function get($id) {
-		$oCity = Parcours::where('id', $id)->first();
-		return $this->sendResponse($oCity->toArray(), null);
-	}
-}
