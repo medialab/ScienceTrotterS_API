7b7204fd11111ce031ccda1f2cb25608768f38b6
karl <fares_k@etna-alternance.net>
2018-05-18 15:08:53 +0100

    dev

--- app/Http/Controllers/PublicControllers/CitiesController.php
+++ app/Http/Controllers/PublicControllers/CitiesController.php
@@ -21,18 +21,3 @@ class CitiesController extends Controller
 		return $this->sendResponse($oCity->toArray(), null);
 	}
 }
-
-
-class ParcoursController extends Controller
-{
-	public function list(Request $oRequest)
-	{
-		$aParcours = Parcours::where('state', true)->take($oRequest->input('limit'))->skip($oRequest->input('offset'))->get();
-		return $this->sendResponse($aParcours->toArray(), null);
-	}
-
-	public function get($id) {
-		$oCity = Parcours::where('id', $id)->first();
-		return $this->sendResponse($oCity->toArray(), null);
-	}
-}
--- routes/public/parcours.php
+++ routes/public/parcours.php
@@ -1,16 +1,4 @@
 <?php
-$p = dirname(__FILE__).'/../../app/Http/Controllers/PublicControllers/ParcoursController.php';
-require_once($p);
-
-use App\Http\Controllers;
-
-
-
-var_dump($p);
-var_dump(file_exists($p));
-
-var_dump(class_exists("ParcoursController"));
-exit;
 
 $router->get('/list', 'ParcoursController@list');
-$router->get('/{id:[a-z0-9-]+}', 'CitiesController@get');
+$router->get('/{id:[a-z0-9-]+}', 'ParcoursController@get');
