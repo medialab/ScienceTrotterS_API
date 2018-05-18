544c86186da0e0bb8d26457e1e55b34582e7f6b2
karl <fares_k@etna-alternance.net>
2018-05-18 14:44:08 +0100

    dev

--- routes/public/parcours.php
+++ routes/public/parcours.php
@@ -1,15 +1,4 @@
 <?php
 
-use App\Http\Controllers\ParcoursController AS Parcours;
-use App\Http\Controllers\CitiesController AS CityCtrl;
-use App\Http\Controllers\UsersController AS UserCtrl;
-
-$router->get('/list', function() {
-	var_dump("Parcours", class_exists("Parcours"));
-	var_dump("Cities", class_exists("CityCtrl"));
-	var_dump("Cities", class_exists("CityCtrl"));
-	var_dump("Users", class_exists("UserCtrl"));
-	exit;
-});
-
-$router->get('/{id:[a-z0-9-]+}', 'ParcoursController@get');
+$router->get('/list', 'CitiesController@list');
+$router->get('/{id:[a-z0-9-]+}', 'CitiesController@get');
