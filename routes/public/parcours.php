b9e4aa2748ada49baa604c22afe91095a5e65f63
karl <fares_k@etna-alternance.net>
2018-05-18 15:02:36 +0100

    dev

--- routes/public/parcours.php
+++ routes/public/parcours.php
@@ -1,24 +1,4 @@
 <?php
-define("DBG_MODE", true);
-ini_set( 'display_errors', true );
-error_reporting( E_ALL | E_NOTICE );
-ini_set('xdebug.var_display_max_data', -1);
-ini_set('xdebug.var_display_max_depth', -1);
-ini_set('xdebug.var_display_max_children', -1);
 
-$p = dirname(__FILE__).'/../../app/Http/Controllers/PublicControllers/ParcoursController.php';
-require_once($p);
-
-use App\Http\Controllers;
-use App\Http\Controllers\Test as Test;
-
-
-
-var_dump($p);
-var_dump(file_exists($p));
-new Test();
-
-exit;
-
-$router->get('/list', 'ParcoursController@list');
+$router->get('/list', 'TestController@list');
 $router->get('/{id:[a-z0-9-]+}', 'CitiesController@get');
