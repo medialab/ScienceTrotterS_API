f9d13d636aa3fb25af5f2bb89a68585e3b07620e
karl <fares_k@etna-alternance.net>
2018-05-18 14:40:34 +0100

    dev

--- routes/public/parcours.php
+++ routes/public/parcours.php
@@ -1,4 +1,11 @@
 <?php
 
-$router->get('/list', 'ParcoursController@list');
+use App\Http\Controllers\ParcoursAdminController;
+
+
+
+$router->get('/list', function() {
+	var_dump(class_exists("ParcoursAdminController"));
+	exit;
+});
 $router->get('/{id:[a-z0-9-]+}', 'ParcoursController@get');
