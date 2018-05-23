4ffa6400dcda699251288bfc5f3a4949b3b57bbb
karl <fares_k@etna-alternance.net>
2018-05-23 10:11:24 +0100

    dev: 23/05/18

--- routes/private/interrests.php
+++ routes/private/interrests.php
@@ -3,33 +3,33 @@
 use App\Utils\APIControllerUtil as Controller;
 use App\Utils\RequestUtil as Request;
 use App\Utils\ValidatorUtil as Validator;
-use App\Models\Interretsts;
+use App\Models\Interrests;
 
 
-class InterretstsAdminController extends Controller
+class InterrestsAdminController extends Controller
 {
 	public function list()
 	{
 		global $GET;
-		$aInterretsts = Interretsts::take((int)$GET['limit'])->skip((int)$GET['offset'])->get();
-		return $this->sendResponse($aInterretsts->toArray(), null)->content();
+		$aInterrests = Interrests::take((int)$GET['limit'])->skip((int)$GET['offset'])->get();
+		return $this->sendResponse($aInterrests->toArray(), null)->content();
 	}
 
 	public function get($id) {
 		global $GET;
-		$oCity = Interretsts::where('id', $id)->first();
+		$oCity = Interrests::where('id', $id)->first();
 		return $this->sendResponse($oCity->toArray(), null)->content();
 	}
 }
 
 $router->get('/list', function() {
-	$ctrl = new InterretstsAdminController();
+	$ctrl = new InterrestsAdminController();
 	echo ($ctrl->list());
 	exit;
 });
 
 $router->get('/{id:[a-z0-9-]+}', function() {
-	$ctrl = new InterretstsAdminController();
+	$ctrl = new InterrestsAdminController();
 	$arr = explode('/', $_SERVER['REQUEST_URI']);
 	$id = $arr[count($arr)-1];
 
