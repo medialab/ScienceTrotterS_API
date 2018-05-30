7ef494051c09779eddec707fa42d7f0057f25ff5
karl <fares_k@etna-alternance.net>
2018-05-30 15:00:14 +0100

    correction nom fichier

---
+++ app/Http/Controllers/PublicControllers/InterestsController.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Utils\APIControllerUtil as Controller;
+use App\Utils\RequestUtil as Request;
+use App\Utils\ValidatorUtil as Validator;
+use App\Models\Interests;
+
+
+class InterestsController extends Controller
+{
+	public function list(Request $oRequest)
+	{	
+		$limit = (int)$oRequest->input('limit');
+		if (!$limit) {
+			$limit = false;
+		}
+		
+		$skip = (int)$oRequest->input('skip');
+		if (!$skip) {
+			$skip = false;
+		}
+
+		$sLang = $oRequest->input('lang');
+		if ($sLang) {
+			$oInterests = Interests::where('state->'.$sLang, 'true')->take($limit)->skip($skip)->get();
+
+			foreach ($oInterests as $key => &$oInt) {
+				$oInt->setLang($sLang);
+			}
+		}
+		else{
+			$oInterests = Interests::take($limit)->skip($skip)->get();
+		}
+
+		return $this->sendResponse($oInterests->toArray(), null)->content();
+	}
+
+	public function get($id) {
+		$oCity = Parcours::where('id', $id)->first();
+		return $this->sendResponse($oCity->toArray(), null)->content();
+	}
+}
