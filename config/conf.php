815649f0da09d3c04c881355c7af4fc33a9142cc
Paul Girard <paul.girard@sciencespo.fr>
2018-08-21 17:57:09 +0200

    conf.php is now ignored to avoid local settings versionning, adding default file back

--- config/conf.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define('API_PASS', 'ZWn%mU5]');
-define('ADMIN_URL', 'https://admin-sts-dev.actu.com/');
-define('PUBLIC_PATH', realpath('.').'/ressources/');
-define('UPLOAD_PATH', PUBLIC_PATH.'upload/');
-
-define('API_ALLOW_ORIGIN', ADMIN_URL);
-
-define('MAP_API_KEY', '5b3ce3597851110001cf624886aa019a35474a2ebba895093897e389');
