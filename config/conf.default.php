d815683816844963ed4157417babeb7cc78743f1
Paul Girard <paul.girard@sciencespo.fr>
2018-09-07 11:34:17 +0200

    API config headache again

--- config/conf.default.php
+++ config/conf.default.php
@@ -4,4 +4,5 @@ define('API_ALLOW_ORIGIN', getenv('API_ALLOW_ORIGIN'));
 define('ADMIN_URL', getenv('BACKOFFICE_URL'));
 define('PUBLIC_PATH', realpath('.').'/ressources/');
 define('UPLOAD_PATH', PUBLIC_PATH.'upload/');
+define('API_SESSION_LIFETIME', getenv(API_SESSION_LIFETIME));
 define('MAP_API_KEY', getenv('MAP_API_KEY'));
