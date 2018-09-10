65ffe6bfe79eb55f35c0b95a8fd86e778111af2c
Paul Girard <paul.girard@sciencespo.fr>
2018-09-10 18:06:50 +0200

    the correct coniguration again...

--- config/conf.default.php
+++ config/conf.default.php
@@ -1,7 +1,9 @@
 <?php
-<?php
-define('API_PASS', env('API_PASS'));
-define('ADMIN_URL', env('BACKOFFICE_URL'));
+define('API_PASS', getenv('API_PASS'));
+define('API_ALLOW_ORIGIN', getenv('API_ALLOW_ORIGIN'));
+define('ADMIN_URL', getenv('BACKOFFICE_URL'));
 define('PUBLIC_PATH', realpath('.').'/ressources/');
+define('UPLOAD_PATH', PUBLIC_PATH.'upload/');
 define('API_SESSION_LIFETIME', getenv('API_SESSION_LIFETIME'));
 define('MAP_API_KEY', getenv('MAP_API_KEY'));
+
